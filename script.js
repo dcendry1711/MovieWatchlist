12f3907dd3c28bcb93548054dde936a6cc458cca
dcendry1711 <daniel.cendry@gmail.com>
2025-06-10 16:48:08 +0200

    displaySearchFilms
    phase1

---
+++ script.js
@@ -0,0 +1,36 @@
+const inputEl = document.getElementById('search-input')
+const searchBtn = document.getElementById('search-button')
+const emptyList = document.getElementById('empty-list')
+const filmList = document.getElementById('film-list')
+
+const filmArray = []
+
+searchBtn.addEventListener('click', addMoviesToLocalArray)
+
+function addMoviesToLocalArray(){
+    emptyList.classList.add('hidden')
+    fetch(`http://www.omdbapi.com/?s=${inputEl.value}&type=movie&apikey=38e170a6`)
+    .then(res => res.json())
+    .then(data => { 
+        searchArray = data.Search.slice(0,5)
+        searchArray.forEach(function(film){
+            fetch(`http://www.omdbapi.com/?t=${film.Title}&type=movie&apikey=38e170a6`)
+                .then(response => response.json())
+                .then(finf =>{
+                    filmList.innerHTML += `
+                    <div class="film-poster">
+                        <img src="${finf.Poster}">
+                    </div>
+                    <div class="film-info">
+                        <h3>${finf.Title}</h3>
+                        <p>${finf.Rangetime}</p>
+                        <p>${finf.Genre}</p>
+                        <button>Watchlist</button>
+                        <p>${finf.Plot}</p>
+                    </div>
+                    `
+                })
+            })
+        })
+}
+     
