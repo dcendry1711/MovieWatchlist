46f774fda8e11714762dbc09d29ef62bcc8fe58f
dcendry1711 <daniel.cendry@gmail.com>
2025-06-10 16:57:59 +0200

    modify emptyList

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const filmArray = []
 searchBtn.addEventListener('click', addMoviesToLocalArray)
 
 function addMoviesToLocalArray(){
-    emptyList.classList.add('hidden')
+    emptyList.style.display = 'none'
     fetch(`http://www.omdbapi.com/?s=${inputEl.value}&type=movie&apikey=38e170a6`)
     .then(res => res.json())
     .then(data => { 
@@ -18,16 +18,19 @@ function addMoviesToLocalArray(){
                 .then(response => response.json())
                 .then(finf =>{
                     filmList.innerHTML += `
-                    <div class="film-poster">
-                        <img src="${finf.Poster}">
+                    <div class="single-movie">
+                        <div class="film-poster">
+                            <img src="${finf.Poster}">
+                        </div>
+                        <div class="film-info">
+                            <h3>${finf.Title}</h3>
+                            <p>${finf.Rangetime}</p>
+                            <p>${finf.Genre}</p>
+                            <button id="watchlist-btn">Watchlist</button>
+                            <p>${finf.Plot}</p>
+                        </div>
                     </div>
-                    <div class="film-info">
-                        <h3>${finf.Title}</h3>
-                        <p>${finf.Rangetime}</p>
-                        <p>${finf.Genre}</p>
-                        <button>Watchlist</button>
-                        <p>${finf.Plot}</p>
-                    </div>
+                    <hr>
                     `
                 })
             })
